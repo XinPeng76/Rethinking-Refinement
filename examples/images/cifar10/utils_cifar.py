44ff732e108a33906b0d9be0140207a2f51089bb
XinPeng76 <xinpeng@bupt.edu.cn>
2026-01-19 15:48:57 +0800

    Initial commit

---
+++ examples/images/cifar10/utils_cifar.py
@@ -0,0 +1,167 @@
+import copy
+import os
+
+import torch
+from torch import distributed as dist
+from torchdyn.core import NeuralODE
+
+# from torchvision.transforms import ToPILImage
+from torchvision.utils import make_grid, save_image
+
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+
+
+def setup(
+    rank: int,
+    total_num_gpus: int,
+    master_addr: str = "localhost",
+    master_port: str = "12355",
+    backend: str = "nccl",
+):
+    """Initialize the distributed environment.
+
+    Args:
+        rank: Rank of the current process.
+        total_num_gpus: Number of GPUs used in the job.
+        master_addr: IP address of the master node.
+        master_port: Port number of the master node.
+        backend: Backend to use.
+    """
+
+    os.environ["MASTER_ADDR"] = master_addr
+    os.environ["MASTER_PORT"] = master_port
+
+    # initialize the process group
+    dist.init_process_group(
+        backend=backend,
+        rank=rank,
+        world_size=total_num_gpus,
+    )
+
+
+def generate_samples(model, parallel, savedir, step, net_="normal", guidance=False):
+    """Save 64 generated images (8 x 8) for sanity check along training.
+
+    Parameters
+    ----------
+    model:
+        represents the neural network that we want to generate samples from
+    parallel: bool
+        represents the parallel training flag. Torchdyn only runs on 1 GPU, we need to send the models from several GPUs to 1 GPU.
+    savedir: str
+        represents the path where we want to save the generated images
+    step: int
+        represents the current step of training
+    """
+    model.eval()
+
+    model_ = copy.deepcopy(model)
+    if parallel:
+        # Send the models from GPU to CPU for inference with NeuralODE from Torchdyn
+        model_ = model_.module.to(device)
+    if guidance == True:
+        y = torch.range(0, 9, device=device).long()
+        y = y.repeat(10)
+        vf = lambda t, x, args=None: model_(t, x, y)
+    node_ = NeuralODE(vf, solver="euler", sensitivity="adjoint")
+    with torch.no_grad():
+        traj = node_.trajectory(
+            torch.randn(100, 3, 32, 32, device=device),
+            t_span=torch.linspace(0, 1, 100, device=device),
+        )
+        traj = traj[-1, :].view([-1, 3, 32, 32]).clip(-1, 1)
+        traj = traj / 2 + 0.5
+    save_image(traj, savedir + f"{net_}_generated_FM_images_step_{step}.png", nrow=10)
+
+    model.train()
+
+def generate_samples2(model, parallel, savedir, step, net_="normal", guidance=False):
+    """Save 64 generated images (8 x 8) for sanity check along training.
+
+    Parameters
+    ----------
+    model:
+        represents the neural network that we want to generate samples from
+    parallel: bool
+        represents the parallel training flag. Torchdyn only runs on 1 GPU, we need to send the models from several GPUs to 1 GPU.
+    savedir: str
+        represents the path where we want to save the generated images
+    step: int
+        represents the current step of training
+    """
+    model.eval()
+
+    model_ = copy.deepcopy(model)
+    if parallel:
+        # Send the models from GPU to CPU for inference with NeuralODE from Torchdyn
+        model_ = model_.module.to(device)
+    if guidance == True:
+        y = torch.range(0, 9, device=device).long()
+        y = y.repeat(10)
+        xT = y.unsqueeze(-1).unsqueeze(-1).unsqueeze(-1).expand(100,3,32,32).to(device)
+        vf = lambda t, x, args=None: model_(t, x, y) + (1-2*t.unsqueeze(-1).unsqueeze(-1).unsqueeze(-1).expand_as(x))*xT
+    node_ = NeuralODE(vf, solver="euler", sensitivity="adjoint")
+    with torch.no_grad():
+        traj = node_.trajectory(
+            torch.randn(100, 3, 32, 32, device=device),
+            t_span=torch.linspace(0, 1, 100, device=device),
+        )
+        traj = traj[-1, :].view([-1, 3, 32, 32]).clip(-1, 1)
+        traj = traj / 2 + 0.5
+    save_image(traj, savedir + f"{net_}_generated_FM_images_step_{step}.png", nrow=10)
+
+    model.train()
+
+def generate_samples_ext(x0,y, model, parallel, savedir, step, net_="normal", guidance=False):
+    """Save 64 generated images (8 x 8) for sanity check along training.
+
+    Parameters
+    ----------
+    model:
+        represents the neural network that we want to generate samples from
+    parallel: bool
+        represents the parallel training flag. Torchdyn only runs on 1 GPU, we need to send the models from several GPUs to 1 GPU.
+    savedir: str
+        represents the path where we want to save the generated images
+    step: int
+        represents the current step of training
+    """
+    model.eval()
+
+    model_ = copy.deepcopy(model)
+    if parallel:
+        # Send the models from GPU to CPU for inference with NeuralODE from Torchdyn
+        model_ = model_.module.to(device)
+    if guidance == True:
+        #y = torch.randint(0, 10, (64,), device=device)
+        #y = torch.range(0, 10, device=device).repeat(7)[:64].long()
+        vf = lambda t, x, args=None: model_(t, x, y)
+    else:
+        vf = model_
+    node_ = NeuralODE(vf, solver="euler", sensitivity="adjoint")
+    x0 = x0.to(device)
+    with torch.no_grad():
+        traj = node_.trajectory(
+            x0,
+            t_span=torch.linspace(0, 1, 100, device=device),
+        )
+        traj = traj[-1, :].view([-1, 3, 32, 32]).clip(-1, 1)
+        traj = traj / 2 + 0.5
+    save_image(traj, savedir + f"{net_}_generated_FM_images_step_{step}.png", nrow=10)
+
+    model.train()
+
+def ema(source, target, decay):
+    source_dict = source.state_dict()
+    target_dict = target.state_dict()
+    for key in source_dict.keys():
+        target_dict[key].data.copy_(
+            target_dict[key].data * decay + source_dict[key].data * (1 - decay)
+        )
+
+
+def infiniteloop(dataloader):
+    while True:
+        for x, y in iter(dataloader):
+            yield x
